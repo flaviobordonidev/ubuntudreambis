8ce9646d1ae6e90f32ee9ef396bf6e8da88f4806
Flavio Bordoni <flavio.bordoni.dev@gmail.com>
2024-02-12 23:01:18 +0100

    set i18n default_locale = :it

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+# Set the default language.
+Rails.application.config.i18n.available_locales = [:en, :it]
+Rails.application.config.i18n.default_locale = :it
+Rails.application.config.i18n.fallbacks = true
